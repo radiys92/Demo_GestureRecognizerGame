1e25a3dd7eb2ab24353e47193950e778c757b97a
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-09-14 01:30:02 +0300

    gesture templates editor

---
+++ GestureRecognizerGameUnity/Assets/Scripts/Helpers/GestureTemplates/GestureTemplates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GestureTemplates", menuName = "Gesture/Templates", order = 1)]
+public class GestureTemplates : ScriptableObject
+{
+    public GestureTemplate[] Templates;
+}
+
+[Serializable]
+public class GestureTemplate
+{
+    public Vector2[] points;
+}
