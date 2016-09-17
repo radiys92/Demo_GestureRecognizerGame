973afa8fea2af27c95ccbc48f1624a21c7cf8234
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-09-18 00:11:16 +0300

    inactive window views activation at startup

---
+++ GestureRecognizerGameUnity/Assets/Scripts/Helpers/WindowsViewStartupActivator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UIView;
+
+public class WindowsViewStartupActivator : MonoBehaviour
+{
+    void Awake()
+    {
+        foreach (var view in transform.GetComponentsInChildren<WindowView>(true))
+        {
+            view.gameObject.SetActive(true);
+        }
+    }
+}
