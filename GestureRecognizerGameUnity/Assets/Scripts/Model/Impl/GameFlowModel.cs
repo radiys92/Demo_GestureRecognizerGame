a57c7a91dc264a54c8a8517d699d6fe7165df193
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-09-17 23:12:19 +0300

    models refactor

--- GestureRecognizerGameUnity/Assets/Scripts/Model/Impl/GameFlowModel.cs
+++ GestureRecognizerGameUnity/Assets/Scripts/Model/Impl/GameFlowModel.cs
@@ -8,20 +8,11 @@ namespace Model.Impl
 {
     public class GameFlowModel : IGameFlowModel
     {
-        public GameFlowModel()
-        {
-            GameState = new HandledProperty<GameStates>(GameStates.None);
-            FirstGesture = new HandledProperty<Gesture>();
-            SecondGesture = new HandledProperty<Gesture>();
-            ComparsionScore = new HandledProperty<float>();
-            LineRenderers = new List<LineRenderer>();
-        }
-
-        public HandledProperty<GameStates> GameState { get; private set; }
-        public HandledProperty<Gesture> FirstGesture { get; private set; }
-        public HandledProperty<Gesture> SecondGesture { get; private set; }
-        public HandledProperty<float> ComparsionScore { get; private set; }
-        public List<LineRenderer> LineRenderers { get; private set; }
+        public HandledProperty<GameStates> GameState { get; } = new HandledProperty<GameStates>(GameStates.None);
+        public HandledProperty<Gesture> FirstGesture { get; } = new HandledProperty<Gesture>();
+        public HandledProperty<Gesture> SecondGesture { get; } = new HandledProperty<Gesture>();
+        public HandledProperty<float> ComparsionScore { get; } = new HandledProperty<float>();
+        public List<LineRenderer> LineRenderers { get; } = new List<LineRenderer>();
     }
 
     public enum GameStates
--- GestureRecognizerGameUnity/Assets/Scripts/Model/Impl/GamePlayModel.cs
+++ GestureRecognizerGameUnity/Assets/Scripts/Model/Impl/GamePlayModel.cs
@@ -6,17 +6,9 @@ namespace Model.Impl
 {
     public class GamePlayModel : IGamePlayModel
     {
-        public HandledProperty<GamePlayState> State { get; private set; }
-        public HandledProperty<int> Score { get; private set; }
-        public HandledProperty<int> Stage { get; private set; }
-        public HandledProperty<TimeSpan> Time { get; private set; }
-
-        public GamePlayModel()
-        {
-            Score = new HandledProperty<int>(0);
-            Stage = new HandledProperty<int>(0);
-            Time = new HandledProperty<TimeSpan>(TimeSpan.FromSeconds(160));
-            State = new HandledProperty<GamePlayState>(GamePlayState.Init);
-        }
+        public HandledProperty<GamePlayState> State { get; } = new HandledProperty<GamePlayState>(GamePlayState.Init);
+        public HandledProperty<int> Score { get; } = new HandledProperty<int>(0);
+        public HandledProperty<int> Stage { get; } = new HandledProperty<int>(0);
+        public HandledProperty<TimeSpan> Time { get; } = new HandledProperty<TimeSpan>(TimeSpan.FromSeconds(0));
     }
 }
